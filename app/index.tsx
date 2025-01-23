e4449427f7d67acc07d8f74c4a4c378c3fc61a73
Priyanshu Desai <pdesai1@seattleu.edu>
2025-01-22 17:58:52 -0800

    refactor: update app layout to use SafeAreaView and add device orientation support

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>EHello World</Text>
-    </View>
-  );
+import React from "react"; 
+import {Dimensions,StyleSheet, Text, View, SafeAreaView, Image, TouchableWithoutFeedback} from "react-native";
+import {useDeviceOrientation } from "@react-native-community/hooks";
+export default function App() {
+  
+ return(
+  <View style={{ flex: 1 }}>
+    <View style={{
+      backgroundColor: "dodgerblue",
+      flex: 1
+    }}></View>
+    <View style={{
+      backgroundColor: "gold",
+      flex: 1
+    }}></View>
+  </View>
+ )
 }
